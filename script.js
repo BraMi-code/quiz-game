7312df645c57f6b5c5c6e83253552acaecaf95cf
Branka Mitrovic <bramit93.gmail.com>
2021-03-19 15:01:25 +0100

    button replaced with input and coloured if true/false

--- script.js
+++ script.js
@@ -28,8 +28,21 @@ var quizQuestions = [
     }
 ];
 
-var quizContainer = document.getElementById('quiz-container');
+const quizContainer = document.getElementById('quiz');
 
+const resultsContainer = document.getElementById('results');
+
+const submitButton =  document.getElementById('submit');
+submitButton.addEventListener('click', showResults);
+
+const nextQuestion = document.getElementById('nextQuestion');
+nextQuestion.addEventListener('click', showNextSlide);
+
+const previousQuestion = document.getElementById('previousQuestion');
+previousQuestion.addEventListener('click', showPrevSlide);
+
+
+/* SOUND
 var mySound = document.getElementById('correct-answer');
 var correctAnswer = document.getElementById('correctAns');
 var wrongAnswer = document.getElementById('wrong-answer');
@@ -41,40 +54,75 @@ var wrong3 = document.getElementById('wrong3');
 function wrongAnswer(e) {
     alert("Incorrect!");
     wrongAnswer.play();
-}
+}  
 
 if (correctAnswer === true) {
     console.log("Correct!");
-}
+} */
 
-function showQuestions(quizQuestions, quiz) {
-    console.log(quizQuestions);
-    var output = [];
-    var answers;
+function playQuiz(quizQuestions) {
+    console.log("Let's start!");
+    // variable to store HTML
+    const output = [];
 
-    for (var i=0; i<quizQuestions.length; i++) {
-        console.log(quizQuestions[i].question);
-        answers = [];
-        for (letter in quizQuestions[i].answers) {
-            console.log(letter);
+    quizQuestions.forEach((currentQuestion, questionNum) => {
+        const answers = [];
+        for (letter in currentQuestion.answers) {
             answers.push(
-                '<button> ' + quizQuestions[i].answers[letter] + '</button>'
+               `<label> 
+               <input type="radio" name="question${questionNum}" value="${letter}"> ${ currentQuestion.answers[letter] } 
+               </label>`
             );
-        }
+        } 
         output.push(
-            '<h3 class="question">' + quizQuestions[i].question + '</h3>' +
-            '<div class="answers">' + answers.join('') + '</div>'
+            `<h3> ${currentQuestion.question} </h3>
+            <div class="answers"> ${answers.join('')} </div>`
         );
-    }
+    });
+
+    // combine output list into one string of HTML and put it on the page
     quizContainer.innerHTML = output.join('');
 }
 
-function playQuiz(quizQuestions) {
-    
-    console.log("play the quiz")
-    showQuestions(quizQuestions);
-    console.log(quizQuestions[0]);
+function showResults() {
+    console.log('results');
+    // gather answer containers from quiz
+    const ansContainers = quizContainer.querySelectorAll('.answers');
 
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    quizQuestions.forEach((currentQuestion, questionNum) => {
+        // find selected answer
+        const answerContainer = ansContainers[questionNum];
+        const selector = `input[name=question${questionNum}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        // if answer is correct
+        if (userAnswer === currentQuestion.correctAns) {
+            numCorrect++;
+            console.log(numCorrect);
+        ansContainers[questionNum].style.color = "lightgreen";
+        }
+        else {
+            ansContainers[questionNum].style.color = "red";
+        }
+    });
+    resultsContainer.innerHTML = `${numCorrect} out of ${quizQuestions.length}`;
+}
+
+function showNextSlide() {
+    console.log("next slide");
+    showSlide(currentSlide + 1);
+}
+
+function showPrevSlide() {
+    console.log("prev");
+    showSlide(currentSlide - 1);
+}
+
+function showSlide(number) {
+    console.log(number);
 }
 
 
